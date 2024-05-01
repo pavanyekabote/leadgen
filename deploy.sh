48664945c8a7b6f5eac385eef66b7a149e7f8748
Pavan Kumar Yekabote <yekabotep@gmail.com>
2024-05-01 23:43:00 +0530

    Feat added deployment script with lambda

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,5 @@ mkdir deployment
 cp -R src/* deployment/
 pip install -t ./deployment/ -r requirements.txt
 cd deployment
-7z a ../SeleniumScraper *
+7z a ../SeleniumScraper.zip *
+aws lambda update-function-code --function-name arn:aws:lambda:us-east-1:667043222635:function:webscraper-lambda  --zip-file fileb://SeleniumScraper.zip
