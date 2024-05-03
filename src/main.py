ccff9d9ae3bb1c3baa3fec8bba8321221d48db0a
Pavan Kumar Yekabote <yekabotep@gmail.com>
2024-05-03 13:24:10 +0530

    Fix: initialize db

--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 from get import method_GET
 from post import method_POST
 import json
-
+from db import MongoDb
 
 def main(event, context):
     print("Event is ", event)
 
+    db = MongoDb.get_instance()
     if event.get("httpMethod") == 'POST':
         return method_POST(event, context)
     elif event.get("httpMethod") == 'GET':
